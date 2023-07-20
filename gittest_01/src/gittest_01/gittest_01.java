0f17155e5de0c3947e7734c822a704bfe8a4c0c8
jiseonkang <138001688+jiseonkang@users.noreply.github.com>
2023-07-20 15:28:47 +0900

    Update gittest_01.java

--- gittest_01/src/gittest_01/gittest_01.java
+++ gittest_01/src/gittest_01/gittest_01.java
@@ -5,6 +5,7 @@ public class gittest_01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이클립스와 깃 연동");
+		System.out.println("이클립스와 깃 연동");
 	}
 
 }
